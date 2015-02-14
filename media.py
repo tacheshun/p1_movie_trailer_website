404dcc4531d227c441bed2df1ea1e8664dac269e
Marius Godeanu-Costache <marius.costache@lofty.com>
2015-02-15 00:16:04 +0200

    Update media.py
    Comment modified

--- media.py
+++ media.py
@@ -2,7 +2,7 @@ import webbrowser
 
 class Movie():
 	"""class representation for Movie"""
-	#constructor class creating the movies with required information
+	#constructor method for assigning required information to movies
 	def __init__(self, title, release_date, story, poster_image_url, trailer_youtube_url):
 		self.title = title
 		self.release_date = release_date
