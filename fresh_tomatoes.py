ee39e251dca617b962846cc76cc0980eda779913
Marius Costache <marius.costache@lofty.com>
2015-02-14 22:51:49 +0200

    added more info

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -122,6 +122,8 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
+    <p>Release Date: {movie_release_date}</p>
+    <p>Storyline: {movie_story_line}</p>
 </div>
 '''
 
@@ -138,7 +140,9 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            movie_release_date=movie.release_date,
+            movie_story_line=movie.story
         )
     return content
 
